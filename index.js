ec6338a68570e871b0f17be1f17f5d29e6622732
Joyin-lidayes <58499288+Joyin-lidayes@users.noreply.github.com>
2021-09-03 13:39:17 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -8,6 +8,88 @@ document.onkeydown = function(e) { // 回车提交表单
 }
 
 /**
+ * 随机生成在游戏目录区间的随机数
+ * @param  min=0  最小值，默认0
+ * @param  max=allGame.length-1  最大值
+ * @return int          返回的结果
+ */
+function randomRange(min = 0, max = allGame.length - 1) {
+    return ~~(Math.random() * (max - min + 1)) + min
+}
+
+function getAllGame() {
+    if (document.getElementById("game")) {
+        document.getElementById("game").remove();
+    }
+    if (document.getElementById("foot")) {
+        document.getElementById("foot").remove();
+    }
+    var bodyFa = document.getElementById("showGame"); //通过id号获取frameLi 的父类
+    var frameUl = document.createElement("ul");
+    frameUl.setAttribute("id", "game");
+    bodyFa.appendChild(frameUl);
+    for (var i = 0; i < allGame.length; i++) {
+        var temp = allGame[i].split(",");
+        var frameLi = document.createElement("li");
+        frameLi.setAttribute("class", "gList");
+        var gameUrl = document.createElement("a");
+        gameUrl.setAttribute("href", temp[2]);
+        gameUrl.setAttribute("class", "urla");
+        frameLi.appendChild(gameUrl);
+        frameUl.appendChild(frameLi);
+        gameUrl.innerHTML = temp[1];
+    }
+    var foot = document.createElement("h3");
+    foot.setAttribute("id", "footer");
+    frameUl.appendChild(foot);
+    foot.innerHTML = "已经到底了！！";
+}
+
+
+/**
+ * 随机生成在游戏目录区间的随机数
+ * @param  randomList  最随机数列表，默认0
+ * @return None        返回的结果
+ */
+function setRandomGame(randomList) {
+    if (document.getElementById("game")) {
+        document.getElementById("game").remove();
+    }
+    if (document.getElementById("foot")) {
+        document.getElementById("foot").remove();
+    }
+    var bodyFa = document.getElementById("showGame"); //通过id号获取frameLi 的父类
+    var frameUl = document.createElement("ul");
+    frameUl.setAttribute("id", "game");
+    bodyFa.appendChild(frameUl);
+    for (var i = 0; i < randomList.length; i++) {
+        var temp = allGame[randomList[i]].split(",");
+        var frameLi = document.createElement("li");
+        frameLi.setAttribute("class", "gList");
+        var gameUrl = document.createElement("a");
+        gameUrl.setAttribute("href", temp[2]);
+        gameUrl.setAttribute("class", "urla");
+        frameLi.appendChild(gameUrl);
+        frameUl.appendChild(frameLi);
+        gameUrl.innerHTML = temp[1];
+    }
+    var foot = document.createElement("h3");
+    foot.setAttribute("id", "footer");
+    frameUl.appendChild(foot);
+    foot.innerHTML = "已经到底了！！";
+}
+
+function randomGame() {
+    randomIt = [];
+    for (var i = 0; i < 15; i++) {
+        temp = randomRange();
+        randomIt.push(temp);
+    }
+    setRandomGame(randomIt);
+
+}
+
+/**
  * 使用indexof方法实现模糊查询
  * @param  {Array}  list     进行查询的数组
  * @param  {String} keyWord  查询的关键词
@@ -22,7 +104,6 @@ function fuzzyQuery(list, keyWord) {
     }
     return arr;
 }
-
 
 function searchGameByName() {
     var gameName = document.getElementById('gameName').value;
